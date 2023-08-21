bfa2f0a232fd2a8cc9d9dd4d86194dd3cbf3249f
alim <mohdalim8180036208@gmail.com>
2023-08-21 21:24:44 +0530

    host added

--- src/router/Router.js
+++ src/router/Router.js
@@ -21,6 +21,7 @@ console.log(smtpUsername, smtpPassword);
 // Configure nodemailer transporter
 const transporter = nodemailer.createTransport({
   service: "outlook",
+  host: "outlook.office365.com",
   auth: {
     user: smtpUsername,
     pass: smtpPassword,
